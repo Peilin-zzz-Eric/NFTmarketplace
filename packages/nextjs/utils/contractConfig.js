6455dee3c4e79f75ffefc17b9adb80641b43bae2
Peilin-zzz-Eric <156723161+Peilin-zzz-Eric@users.noreply.github.com>
2024-12-10 16:27:40 -0500

    Update contractConfig.js

--- packages/nextjs/utils/contractConfig.js
+++ packages/nextjs/utils/contractConfig.js
@@ -1,4 +1,4 @@
-export const NFT_FACTORY_ADDRESS = "0x63B2c9d83adDff9Eff67176dC72DAdDdF275A702";
+export const NFT_FACTORY_ADDRESS = "0xC347874E321Fe535D2b49aeDeE45e5C17e3050C9";
 export const NFT_FACTORY_ABI = [
     "event NFTContractDeployed(address indexed,address indexed)",
     "function deployNFT(string,string)",
